f5414956e61d620fbeb0bf49b713e35c1ada5a6e
Erich Seamon <erichs@uidaho.edu>
2017-04-04 23:18:23 -0700

    alterations

--- neural-networks-R_rev2.R
+++ neural-networks-R_rev2.R
@@ -1,12 +1,18 @@
 
 set.seed(500)
 library(MASS)
+library(boot)
+
 data <- myfiles_allyears[1:7]
 
 index <- sample(1:nrow(data),round(0.75*nrow(data)))
 train <- data[index,]
 test <- data[-index,]
 lm.fit <- glm(countratio~., data=train)
+
+LMCVerror <- cv.glm(train,lm.fit,K=10)$delta[1]
+
+
 summary(lm.fit)
 pr.lm <- predict(lm.fit,test)
 MSE.lm <- sum((pr.lm - test$countratio)^2)/nrow(test)
@@ -24,4 +30,30 @@ n <- names(train_)
 f <- as.formula(paste("countratio ~", paste(n[!n %in% "countratio"], collapse = " + ")))
 nn <- neuralnet(f,data=train_,hidden=c(5,3),linear.output=T)
 plot(nn)
+
+
+cv.error <- NULL
+k <- 10
+
+library(plyr) 
+pbar <- create_progress_bar('text')
+pbar$init(k)
+
+for(i in 1:k){
+  index <- sample(1:nrow(data),round(0.9*nrow(data)))
+  train.cv <- scaled[index,]
+  test.cv <- scaled[-index,]
+  
+  nn <- neuralnet(f,data=train.cv,hidden=c(5,2),linear.output=T)
+  
+  pr.nn <- compute(nn,test.cv[,1:6])
+  pr.nn <- pr.nn$net.result*(max(data$countratio)-min(data$countratio))+min(data$countratio)
+  
+  test.cv.r <- (test.cv$countratio)*(max(data$countratio)-min(data$countratio))+min(data$countratio)
+  
+  cv.error[i] <- sum((test.cv.r - pr.nn)^2)/nrow(test.cv)
+  
+  pbar$step()
+}
+
  
